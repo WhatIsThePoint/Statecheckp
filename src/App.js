f8ae12d5da957981eb67f9f1892e0b8bb07be4a4
YoussefHammi <youssef.hammi.oa@gmail.com>
2020-10-29 11:26:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import react ,{Component} from 'react';
 import './App.css';
+import sung from "./sung.png"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+      FullName:"Youssef Hammi",
+      Bio:"17Yo student at Ali Douagi HighSchool ",
+      ImgSrc:sung,
+      ImgAlt:"HeeHee",
+      Job: "Game And Web Dev",
+      Time:0,
+      show:false
+  };
+
+  }
+   
+
+
+  handelClick=() => this.setState({show : !this.state.show})
+  componentDidMount = () => {
+    setInterval(()=>{
+      let Time = new Date()
+      Time=Time.getHours()+":"+Time.getMinutes()
+      this.setState({Time:Time})
+    },1000)
+  }
+
+render(){
+  return(
+    <>
+     <center>
+    {
+        this.state.show &&
+       
+    <div>
+   
+    <h1>{this.state.FullName}</h1>
+  <h2>{this.state.Bio}</h2>
+  <h2>{this.state.Job}</h2>
+  <img src={this.state.ImgSrc} alt={this.state.ImgAlt} />
+  <br />
+  <h2>{this.state.Time}</h2>
+  
+  </div>
+    }
+<button onClick={this.handelClick}>ENZEL!!!</button>
+  </center>  
+  
+ 
+  </>
+   );
 }
-
-export default App;
+}
+export default App ;
